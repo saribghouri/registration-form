a21d86e5fc8203f93ad79521283ddadc56b299dc
saribghouri <sarib.ghouri92@gmail.com>
2022-10-03 15:42:55 +0500

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Index from "./component";
 
-
 function App() {
   return (
-    <div>  
-      <Index/>
+    <div>
+      <Index />
       {/* <RegistrationForm/> */}
     </div>
   );
 }
 
 export default App;
-
-
-
--- src/component/index.jsx
+++ src/component/index.jsx
@@ -7,55 +7,95 @@ const Index = () => {
     email: "",
     password: "",
   });
-  const [submitted, setSubmitted] = useState(false);
+  const [submitted, setSubmitted] = useState(true);
+  const [firstName, setfirstName] = useState(true);
+  const [lastName, setlastName] = useState(true);
+  const [email, setemail] = useState(true);
+  const [password, setpassword] = useState(true);
 
   const [valid, setValid] = useState(false);
   const handleFirstName = (event) => {
     event.persist();
-    setValues((value) => ({
-      ...value,
-      firstName: event.target.value,
-    }));
+    if (event.target.value) {
+      setValues((value) => ({
+        ...value,
+        firstName: event.target.value,
+      }));
+      setfirstName(true);
+    } else {
+      setfirstName(false);
+    }
   };
   const handleLastName = (event) => {
     event.persist();
-    setValues((value) => ({
-      ...value,
-      lastName: event.target.value,
-    }));
+    if (event.target.value) {
+      setValues((value) => ({
+        ...value,
+        lastName: event.target.value,
+      }));
+      setlastName(true);
+    } else {
+      setlastName(false);
+    }
   };
   const handleEmail = (event) => {
     event.persist();
-    setValues((value) => ({
-      ...value,
-      email: event.target.value,
-    }));
+    if (event.target.value) {
+      setValues((value) => ({
+        ...value,
+        email: event.target.value,
+      }));
+      setemail(true);
+    } else {
+      setemail(false);
+    }
   };
   const handlePasssword = (event) => {
     event.persist();
-    setValues((value) => ({
-      ...value,
-      password: event.target.value,
-    }));
+    if (event.target.value) {
+      setValues((value) => ({
+        ...value,
+        password: event.target.value,
+      }));
+      setpassword(true);
+    } else {
+      setpassword(false);
+    }
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (value.firstName && value.lastName && value.email && value.password) {
       setValid(true);
+      setfirstName(true);
+      setlastName(true);
+      setemail(true);
+      setpassword(true);
+      setValues({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      });
+    } else {
+      if (!value.firstName) {
+        setfirstName(false);
+      }
+      if (!value.lastName) {
+        setlastName(false);
+      }
+      if (!value.email) {
+        setemail(false);
+      }
+      if (!value.password) {
+        setpassword(false);
+      }
     }
-
-    setValues({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    });
-    setSubmitted(false);
+    setSubmitted(true);
   };
   return (
     <div class="form-container">
       <form class="register-form" onSubmit={handleSubmit}>
-        {setValid && valid ? (
+        {submitted && valid ? (
           <div class="success-message">Successfull Thank you!</div>
         ) : null}
         <input
@@ -67,7 +107,8 @@ const Index = () => {
           name="firstName"
           onChange={handleFirstName}
         />
-        {submitted && !value.firstName && <span>Enter a first name</span>}
+        {/* {submitted && !value.firstName && <span>Enter a first name</span>} */}
+        {firstName != true ? <span>Enter a first name</span> : null}
         <input
           value={value.lastName}
           id="last-name"
@@ -77,7 +118,8 @@ const Index = () => {
           name="lastName"
           onChange={handleLastName}
         />
-        {submitted && !value.lastName && <span>Enter a last name</span>}
+
+        {lastName != true ? <span>Enter a last name</span> : null}
         <input
           value={value.email}
           id="email"
@@ -87,7 +129,7 @@ const Index = () => {
           name="email"
           onChange={handleEmail}
         />
-        {submitted && !value.email && <span> Enter an email address</span>}
+        {email != true ? <span>Enter a email</span> : null}
         <input
           value={value.password}
           id="password"
@@ -97,7 +139,7 @@ const Index = () => {
           name="password"
           onChange={handlePasssword}
         />
-        {submitted && !value.password && <span> Enter a password</span>}
+        {password != true ? <span>Enter a password</span> : null}
         <button class="form-field" type="submit">
           Register
         </button>
